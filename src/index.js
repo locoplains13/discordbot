80b89c4d1b8b4150b3ca2433e02716e31084a032
Angel Roberto Andrade <alroc1234@hotmail.com>
2023-08-23 16:41:19 -0600

    added libraries needed for music bot

--- src/index.js
+++ src/index.js
@@ -19,8 +19,20 @@ client.on("messageCreate", (msg) => {
   if (msg.author.bot) {
     return;
   }
-  if (msg.content === "hey") {
-    msg.reply("hey");
+  if (msg.content === "puto") {
+    msg.reply("puta tu kola");
+  }
+  if (msg.content === "cuando murio halo?") {
+    msg.reply("en reach");
+  }
+  if (msg.content === "jose review") {
+    msg.reply("me caga ese we");
+  }
+  if (msg.content === "angel review") {
+    msg.reply("ese we es mi apa");
+  }
+  if (msg.content === "rigo review") {
+    msg.reply("ese kabron wele a sebolla");
   }
   console.log(`${msg.author.username} said ${msg.content}`);
 });
